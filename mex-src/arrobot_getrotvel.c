337c89522cad8036a6584eed8abc2fc247459fe0
Reed Hedges <reed.hedges@adept.com>
2012-12-11 11:31:36 -0500

    added mex functions for getvel, getrotvel, getlatvel. deleting backup file.

---
+++ mex-src/arrobot_getrotvel.c
@@ -0,0 +1,18 @@
+
+
+/* MEX function for Matlab */
+
+#include "mex.h"
+#include "ariac.h"
+
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+	if(nlhs != 1) {
+		mexErrMsgIdAndTxt("arrobot_getrotvel", "One output required.");
+		return;
+	}
+	plhs[0] = mxCreateDoubleMatrix(1, 1, mxREAL);
+    *(mxGetPr(plhs[0])) = arrobot_getrotvel();
+}
+	
