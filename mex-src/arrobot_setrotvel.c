5827d0ceb10f164fb435d0ba954d8d67024736f5
Reed Hedges <reed.hedges@adept.com>
2013-02-22 16:51:54 -0500

    removed C++ // comments from mex-src files (which fail to compile with strict C compiler on Linux)

--- mex-src/arrobot_setrotvel.c
+++ mex-src/arrobot_setrotvel.c
@@ -9,13 +9,11 @@
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     double *p;
-    //mexPrintf("arrobot_setrotvel mex function start\n");
 	if(nrhs != 1) {
 		mexErrMsgIdAndTxt("arrobot_setrotvel", "One input required.");
 		return;
 	}
     p = mxGetPr(prhs[0]);
-    //mexPrintf("arrobot_setrotvelx mex function got double %f\n", *p);
     arrobot_setrotvel(*p);
 }
 	
